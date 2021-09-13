d409a23b63f08ecb68a4ab9e9ef374429d7b043c
ease <easeanger@gmail.com>
2021-09-13 13:05:37 +0530

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("./client/build"));
   
 }
 //it must be below cors
